3b4cc8ac5ef4183eea587e60d525883d636e083c
Alfghard <alfghardg@gmail.com>
2025-05-17 20:52:59 +0200

    adding routes

---
+++ index.ts
@@ -0,0 +1,24 @@
+import figlet from 'figlet'
+
+const server = Bun.serve({
+    port: 3000,
+    fetch(req){
+        const url = new URL(req.url)
+
+        if(url.pathname== '/'){
+            const body = figlet.textSync("Hello World!")
+        return new Response(body)
+        }
+        if(url.pathname=== '/about'){
+            return new Response("About me!")
+        }
+        if(url.pathname ==='/contact'){
+            return new Response("Contact Us!")
+        }
+
+        return new Response('404!')
+        
+    }
+})
+
+console.log('Listening on PORT http://localhost:${server.port)')
