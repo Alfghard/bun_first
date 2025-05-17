e5c53213a0bc2514246e5888e058b2053de558e9
Alfghard <alfghardg@gmail.com>
2025-05-17 21:30:19 +0200

    rest api

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import figlet from 'figlet'
 
 const server = Bun.serve({
-    port: 3000,
+    port: 3000, //port de sortie
     fetch(req){
         const url = new URL(req.url)
-
+        
+        // routes depending on the pathname
         if(url.pathname== '/'){
             const body = figlet.textSync("Hello World!")
         return new Response(body)
@@ -15,10 +16,28 @@ const server = Bun.serve({
         if(url.pathname ==='/contact'){
             return new Response("Contact Us!")
         }
-
-        return new Response('404!')
+        if(url.pathname ==='/greet'){
+            return new Response(Bun.file('./greet.txt'))
+        }
+        // handle Error
+        if(url.pathname === "/feed"){
+            throw new Error('Could not fetch feed')
+        }
+        return new Response(`404!`)
         
+    },
+    
+    error(error){
+        // in case of error, run this
+        return new Response(`<pre> $(error) \n ${error.stack} </pre>`, {
+            headers: {
+                'Content-Type': 'text/html'
+            }
+        } )
     }
 })
 
-console.log('Listening on PORT http://localhost:${server.port)')
+// use `` to substitute variables with $
+console.log(`Listening on PORT http://localhost:${server.port}`)
+
+
